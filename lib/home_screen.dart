094960b9ce4f89c09ff9bc4cb5ae50a57a7f040b
Matthew-Dev30 <179122635+Matthew-Dev30@users.noreply.github.com>
2025-05-26 23:36:09 -0700

    added bg

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,75 +13,90 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              SizedBox(height: 40),
+        child: Stack(
+          children: [
+            Image.asset(
+              "assets/b.png",
+              height: double.infinity,
+              fit: BoxFit.fitHeight,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  SizedBox(height: 40),
 
-              // Image.asset("assets/logo.png", width: 200, height: 200),
+                  // Image.asset("assets/logo.png", width: 200, height: 200),
 
-              // SizedBox(height: 32),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: ListTile(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => VerificationScreen(),
+                  // SizedBox(height: 32),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => VerificationScreen(),
+                          ),
+                        );
+                      },
+                      title: Text(
+                        "Continue for verification",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
-                    );
-                  },
-                  title: Text(
-                    "Continue for verification",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                  trailing: Icon(Icons.arrow_forward_ios_outlined),
-                ),
+
+                  // SizedBox(height: 20),
+                  // Container(
+                  //   decoration: BoxDecoration(
+                  //     border: Border.all(color: Colors.grey),
+                  //     borderRadius: BorderRadius.circular(20),
+                  //   ),
+                  //   child: ListTile(
+                  //     onTap: () {
+                  //       Navigator.push(
+                  //         context,
+                  //         MaterialPageRoute(builder: (context) => SellerScreen()),
+                  //       );
+                  //     },
+                  //     title: Text(
+                  //       "Continue as a Seller",
+                  //       style: TextStyle(fontWeight: FontWeight.bold),
+                  //     ),
+                  //     trailing: Icon(Icons.arrow_forward_ios_outlined),
+                  //   ),
+                  // ),
+
+                  // SizedBox(height: 20),
+                  // Container(
+                  //   decoration: BoxDecoration(
+                  //     border: Border.all(color: Colors.grey),
+                  //     borderRadius: BorderRadius.circular(20),
+                  //   ),
+                  //   child: ListTile(
+                  //     onTap: () {},
+                  //     title: Text(
+                  //       "Continue as a User",
+                  //       style: TextStyle(fontWeight: FontWeight.bold),
+                  //     ),
+                  //     trailing: Icon(Icons.arrow_forward_ios_outlined),
+                  //   ),
+                  // ),
+                ],
               ),
-
-              // SizedBox(height: 20),
-              // Container(
-              //   decoration: BoxDecoration(
-              //     border: Border.all(color: Colors.grey),
-              //     borderRadius: BorderRadius.circular(20),
-              //   ),
-              //   child: ListTile(
-              //     onTap: () {
-              //       Navigator.push(
-              //         context,
-              //         MaterialPageRoute(builder: (context) => SellerScreen()),
-              //       );
-              //     },
-              //     title: Text(
-              //       "Continue as a Seller",
-              //       style: TextStyle(fontWeight: FontWeight.bold),
-              //     ),
-              //     trailing: Icon(Icons.arrow_forward_ios_outlined),
-              //   ),
-              // ),
-
-              // SizedBox(height: 20),
-              // Container(
-              //   decoration: BoxDecoration(
-              //     border: Border.all(color: Colors.grey),
-              //     borderRadius: BorderRadius.circular(20),
-              //   ),
-              //   child: ListTile(
-              //     onTap: () {},
-              //     title: Text(
-              //       "Continue as a User",
-              //       style: TextStyle(fontWeight: FontWeight.bold),
-              //     ),
-              //     trailing: Icon(Icons.arrow_forward_ios_outlined),
-              //   ),
-              // ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
